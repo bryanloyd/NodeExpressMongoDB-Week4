6f4a0b69b83fc381fada98140b1f31edb4843f90
Bryan Loyd <bryan.loyd@outlook.com>
2021-09-20 04:41:52 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
-}
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "878861956388596",
+    clientSecret: "5b9f543d11d83e12fbaf2c602c5d7729",
+  },
+};
